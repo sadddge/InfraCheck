03b5475b60ee0c8bde4939233f4cf7d3066da988
sadddge <m.sanhueza22@ufromail.cl>
2025-05-22 19:18:56 -0400

    fix(database): correct environment variable for database username

--- backend/src/database/config/database.config.ts
+++ backend/src/database/config/database.config.ts
@@ -5,7 +5,7 @@ export default registerAs('database', (): TypeOrmModuleOptions => ({
     type: 'postgres',
     host: process.env.DB_HOST,
     port: +(process.env.DB_PORT ?? 5432),
-    username: process.env.DB_USERNAME,
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     autoLoadEntities: true,
