9e0ef46e889f11a7e653dfbd9c046410007a595c
sadddge <m.sanhueza22@ufromail.cl>
2025-05-22 18:30:11 -0400

    feat: add throttler module and guard for rate limiting

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,15 +2,30 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
-    })
+    }),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 10,
+        }
+      ]
+    }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+    AppService
+  ],
 })
 export class AppModule {}
