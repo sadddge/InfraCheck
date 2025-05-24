b1067b7dba2483bf93cf997b453e483073afae69
sadddge <m.sanhueza22@ufromail.cl>
2025-05-24 00:17:02 -0400

    feat(auth): implement JwtAuthGuard and RolesGuard for enhanced authorization

--- backend/src/modules/auth/guards/jwt-auth.guard.ts
+++ backend/src/modules/auth/guards/jwt-auth.guard.ts
@@ -1,3 +1,31 @@
+import { ExecutionContext, Injectable, Logger } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
 
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+    private readonly logger = new Logger(JwtAuthGuard.name);
+    constructor(private reflector: Reflector) {
+        super();
+    }
+
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        const isPublic = this.reflector.getAllAndOverride<boolean>('isPublic', [
+            context.getHandler(),
+            context.getClass(),
+        ]);
+        this.logger.debug(`isPublic: ${isPublic}`);
+        if (isPublic) {
+            return true;
+        }
+        return super.canActivate(context);
+    }
+
+    handleRequest<TUser = any>(err: any, user: any, info: any, context: ExecutionContext, status?: any): TUser {
+        if (err || !user) {
+            throw err || new Error('Unauthorized');
+        }
+        return user;
+    }
+}
