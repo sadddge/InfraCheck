3f9ecc1f3ced8b8672336de4ca0d61f6bc8bd8b5
sadddge <m.sanhueza22@ufromail.cl>
2025-06-01 22:25:29 -0400

    feat(auth): add validation for phoneNumber and code fields in VerifyRecoverPasswordDto

--- backend/src/modules/auth/dto/verify-recover-password.dto.ts
+++ backend/src/modules/auth/dto/verify-recover-password.dto.ts
@@ -1,4 +1,9 @@
+import { IsPhoneNumber, IsString, Length } from "class-validator";
+
 export class VerifyRecoverPasswordDto {
+    @IsPhoneNumber('CL')
     phoneNumber: string;
+    @IsString()
+    @Length(6, 6, { message: 'Code must be exactly 6 characters long.' })
     code: string;
 }
