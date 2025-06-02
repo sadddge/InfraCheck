d67b89c1c40323bf17b724048b12d14c2da12f20
sadddge <m.sanhueza22@ufromail.cl>
2025-06-01 21:38:57 -0400

    fix(auth): update import for Reflector to use regular import

--- backend/src/modules/auth/guards/jwt-auth.guard.ts
+++ backend/src/modules/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,5 @@
 import { type ExecutionContext, Injectable, Logger, UnauthorizedException } from '@nestjs/common';
-import type { Reflector } from '@nestjs/core';
+import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import type { Observable } from 'rxjs';
 
