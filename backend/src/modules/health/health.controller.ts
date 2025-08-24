12aec185a116b579511c17dfc8686f0df5826fe2
sadddge <m.sanhueza22@ufromail.cl>
2025-08-24 19:08:33 -0400

    feat(health): add Public decorator to checkHealth endpoint

--- backend/src/modules/health/health.controller.ts
+++ backend/src/modules/health/health.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get, HttpCode, HttpStatus } from "@nestjs/common";
+import { Public } from "src/common/decorators/public.decorator";
 
 @Controller()
 export class HealthController {
     @Get()
+    @Public()
     @HttpCode(HttpStatus.OK)
     checkHealth() {
         return true;
